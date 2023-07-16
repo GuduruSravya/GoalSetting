0084aaaec4214b40f030a270e6e99fa127ae1f00
Sravya <gudursravya@gmail.com>
2023-07-16 16:23:05 -0500

    Initial commit with backend support

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { getGoals,setGoals,updateGoals,deleteGoals } = require('../controllers/goalsController');
+
+
+//Better way
+
+router.route("/").get(getGoals).post(setGoals);
+router.route("/:id").put(updateGoals).delete(deleteGoals);
+
+
+/* One-way
+router.get('/',getGoals)
+
+router.post('/',setGoals)
+
+router.put('/:id',updateGoals)
+
+router.delete('/:id',deleteGoals)*/
+
+module.exports = router

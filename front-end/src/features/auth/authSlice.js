5808a0bc667c71061f91d7f64cbe4726507cf965
Sravya <gudursravya@gmail.com>
2023-07-19 18:02:00 -0500

    Final deployment

--- front-end/src/features/auth/authSlice.js
+++ front-end/src/features/auth/authSlice.js
@@ -17,7 +17,10 @@ export const register = createAsyncThunk('auth/register',async (user,thunkAPI)=>
     try{
         return await authService.register(user);
     }catch(error){
-        const message = (error.response && error.response.data && error.reponse.data.message) || error.message || error.toString()
+        const message = (error.response && error.response.data && error.response.data.message) ||
+        error.message ||
+        error.toString()
+        
         return thunkAPI.rejectWithValue(message)
     }
 })
